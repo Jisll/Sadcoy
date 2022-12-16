3640b215935b7baeccf979dbf7b59f63d01bb4a3
Maurice <69192557+Jisll@users.noreply.github.com>
2022-12-16 01:23:20 +0100

    Update MainWindow.xaml.cs

--- Sadcoy/MainWindow.xaml.cs
+++ Sadcoy/MainWindow.xaml.cs
@@ -1,14 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -19,66 +15,73 @@ namespace Sadcoy
 {
     public partial class MainWindow : Window
     {
-        public MainWindow() => InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
         private void RunSw_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Optimizing started!");
             Optimize.Optimizing();
-            string TempPath = System.IO.Path.GetTempPath();
-            var Dir = new DirectoryInfo(TempPath);
+            string tempPath = System.IO.Path.GetTempPath();
+            var dir = new DirectoryInfo(tempPath);
 
-            if (!Dir.Exists)
+            if (!dir.Exists)
             {
                 MessageBox.Show("Can't Find Temp Folder");
                 return;
             }
 
-            foreach (DirectoryInfo dir in Dir.GetDirectories())
-            {
-                try
-                {
-                    dir.Delete(true);
-                }
-                catch (Exception)
-                {
+            ClearTempFolder(dir);
 
-                }
-            }
-
-            foreach (FileInfo filez in Dir.GetFiles())
-            {
-                try
-                {
-                    filez.Delete();
-                }
-                catch (Exception)
-                {
-
-                }
-            }
             if (MessageBox.Show("You need to restart the computer to apply the effects, do you want to restart now?", "Sadcoy", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK)
             {
                 Process.Start("shutdown.exe", "-r -t 00");
             }
-            else
+        }
+
+        private void ClearTempFolder(DirectoryInfo dir)
+        {
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
             {
-                // nothing
+                try
+                {
+                    subDir.Delete(true);
+                }
+                catch (Exception)
+                {
+                    // Ignore
+                }
+            }
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                }
+                catch (Exception)
+                {
+                    // Ignore
+                }
             }
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
-            => Environment.Exit(0);
+        {
+            Environment.Exit(0);
+        }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
-            => this.DragMove();
+        {
+            this.DragMove();
+        }
 
         private void SupportMe_Click(object sender, RoutedEventArgs e)
-            => System.Diagnostics.Process.Start(new ProcessStartInfo
-            {
-                FileName = "https://github.com/Jisll/Sadcoy",
-                UseShellExecute = true
-            });
+        {
+            Process.Start("https://github.com/Jisll/Sadcoy");
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -87,11 +90,8 @@ namespace Sadcoy
             {
                 Process.Start("shutdown.exe", "-r -t 00");
             }
-            else
-            {
-                // nothing
-            }
         }
+
         private void services_Click(object sender, RoutedEventArgs e)
         {
             Services sv = new Services();
